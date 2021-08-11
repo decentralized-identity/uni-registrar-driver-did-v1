d0776f2c75cdf73a646d4794c497065ef4a4843d
Markus Sabadello <markus@danubetech.com>
2021-08-11 19:31:45 +0400

    fix: Compatibility with latest key-formats-java.

--- src/main/java/uniregistrar/driver/did/v1/DidV1Driver.java
+++ src/main/java/uniregistrar/driver/did/v1/DidV1Driver.java
@@ -12,6 +12,7 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.danubetech.keyformats.jose.JWK;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,7 +21,6 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.databind.node.TextNode;
-import com.nimbusds.jose.jwk.JWK;
 
 import io.leonard.Base58;
 import uniregistrar.RegistrationException;
@@ -233,7 +233,7 @@ public class DidV1Driver extends AbstractDriver implements Driver {
 				if (log.isDebugEnabled()) log.debug("Found key: " + keyUrl);
 
 				keysDocumentJsonKey.put("id", keyUrl);
-				keysDocumentJsonKey.putPOJO("privateKeyJwk", jsonWebKey.toJSONObject());
+				keysDocumentJsonKey.putPOJO("privateKeyJwk", jsonWebKey.toMap());
 
 				keysDocumentJsonKeys.put(keyUrl, keysDocumentJsonKey);
 			}
